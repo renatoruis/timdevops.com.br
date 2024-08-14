78bcdd2fb2001238ebbb3addc8ca5e18c6124862
Tim <renatoruis@gmail.com>
2024-08-14 17:21:34 -0300

    ajustes search

--- docs/js/search.js
+++ docs/js/search.js
@@ -4,6 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   searchInput.addEventListener("input", function () {
     const query = searchInput.value.toLowerCase();
+
+    if (query.length === 0) {
+      searchResults.style.display = "none";
+      searchResults.innerHTML = ""; // Limpar os resultados
+      return;
+    }
+
     fetch("/index.json")
       .then((response) => response.json())
       .then((pages) => {
@@ -13,11 +20,18 @@ document.addEventListener("DOMContentLoaded", function () {
             page.content.toLowerCase().includes(query)
           );
         });
-        searchResults.innerHTML = results
-          .map((result) => {
-            return `<a href="${result.link}" class="block text-blue-500 hover:underline">${result.title}</a>`;
-          })
-          .join("");
+
+        if (results.length > 0) {
+          searchResults.style.display = "block";
+          searchResults.innerHTML = results
+            .map((result) => {
+              return `<a href="${result.link}" class="block text-blue-500 hover:underline">${result.title}</a>`;
+            })
+            .join("");
+        } else {
+          searchResults.style.display = "none";
+          searchResults.innerHTML = ""; // Limpar os resultados
+        }
       });
   });
 });
